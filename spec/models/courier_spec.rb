dc21c39e3191068ab0806fdbc3cb94f16ca66ca3
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-12 19:47:08 -0300

    Refactor specs from original repo, User has_one Customer and Courier(no more IST)

--- spec/models/courier_spec.rb
+++ spec/models/courier_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Courier do
   context 'associations' do
+    it { should belong_to(:users) }
     it { should have_many(:orders) }
     it { should have_many(:vehicles) }
   end
