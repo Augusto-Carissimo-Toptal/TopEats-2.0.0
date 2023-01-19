825f9ff7197b0d24886ea264cfcc6900f7cce6f1
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-19 16:57:56 -0300

    Create db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,41 @@
 require_relative '../lib/populator_fix.rb'
 
-User.populate 100 do |u|
-  u.full_name = Faker::Name.first_name
-  u.phone_number = Faker::Alphanumeric.alpha(number: 10)
-  u.email = Faker::Internet.email
-  address = Address.create(address_field: Faker::Address.street_address)
-  UserAddress.populate 1 do |i|
-    i.user_id = u.id, 
-    i.address_id = address.id
-  end
-end
+1.upto(100) do |num|
+  user = User.create!(
+    full_name: Faker::Name.first_name, 
+    phone_number: Faker::PhoneNumber.cell_phone,
+    email: Faker::Internet.email)
 
-(1...100).each do |i|
-  if i.odd?
-    Customer.create!(user_id: i)
+  address = Address.create!(
+    address_field: Faker::Address.street_address)
+
+  UserAddress.create!(
+    user_id: user.id, 
+    address_id: address.id)
+
+  @menu_item = MenuItem.create!(name: Faker::Food.dish)
+
+  if num.odd?
+    @customer = Customer.create!(
+      user_id: user.id)
   else
-    courier = Courier.create!(user_id: i)
-    # Vehicle.populate 1 do |u|
-    #   u.courier_id = courier.id, 
-    #   u.vehicle_type = ['car', 'bicycle', 'motorcycle'].sample
-    # end
+    courier = Courier.create!(
+      user_id: user.id)
+
+    Vehicle.create!(
+      courier_id: courier.id, 
+      vehicle_type: ['car', 'bicycle', 'motorcycle'].sample)
+
+    order = Order.create!(
+      status: ['pending', 'accepted', 'preparing', 'delivered'].sample,
+      total_price: Faker::Number.decimal(l_digits: 2),
+      courier_id: courier.id,
+      customer_id: @customer.id
+      )
+
+    OrderMenu.create!(
+      order_id: order.id,
+      menu_item_id: @menu_item.id
+    )
   end
 end
-
-Order.populate 100 do |u|
-  (1...100).each do |i|
-    u.status = ['pending', 'accepted', 'preparing', 'delivered' ].sample, 
-    u.total_price = Faker::Number.decimal(l_digits: 2), 
-    u.customer_id = i, 
-    u.courier_id = i 
-  end
-end
-
-# MenuItem.populate 100 do |u|
-#   u.name = Faker::Food.dish
-# end
-
-# Order.populate 25 do |u|
-#   u.status = 'pending'
-#   u.total_price = Faker::Number.decimal(l_digits: 2)
-#   u.courier_id = rand(1...25)
-#   u.customer_id = rand(1...25)
-# end
-
-# Order.populate 25 do |u|
-#   u.status = 'accepted'
-#   u.total_price = Faker::Number.decimal(l_digits: 2)
-#   u.courier_id = rand(26...50)
-#   u.customer_id = rand(26...50)
-# end
-
-# Order.populate 25 do |u|
-#   u.status = 'preparing'
-#   u.total_price = Faker::Number.decimal(l_digits: 2)
-#   u.courier_id = rand(51...75)
-#   u.customer_id = rand(51...75)
-# end
-
-# Order.populate 25 do |u|
-#   u.status = 'delivered'
-#   u.total_price = Faker::Number.decimal(l_digits: 2)
-#   u.courier_id = rand(76...100)
-#   u.customer_id = rand(76...100)
-# end
-
-
-
