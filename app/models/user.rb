edfda43d9db6766b8064eae0fe4875ef38dcea1d
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-20 09:25:07 -0300

    Create UserType

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :full_name
   validates_length_of :full_name, maximum: 250, on: :create
   validates_presence_of :phone_number
-  validates_format_of :phone_number, with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-  message: "- Phone numbers must be in xxx-xxx-xxxx format."
+  
   validates_presence_of :email
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 
